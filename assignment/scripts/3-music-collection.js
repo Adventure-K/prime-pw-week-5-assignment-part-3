71a9a23b7d732f2a0a248324bff3611be69362bc
Andrew Kane <andrew.kane@gmail.com>
2022-07-07 17:40:45 -0500

    Added and tested function findArtist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -26,26 +26,31 @@ console.log(deck);
 
 let collection = [];
 
-
+// Function addToCollection
 function addToCollection(title, artist, yearPublished) {
         let album = {
-            title,
-            artist,
-            yearPublished
+            title: title,
+            artist: artist,
+            yearPublished: yearPublished
         }
     collection.push(album);
     console.log(album, ' has been added to the collection.');
     return album;
     } // End function addToCollection
 
+// collection population
 addToCollection('Symphony and Metallica', 'Metallica', 1999);
 addToCollection('Monteverdi: Vespers of the Blessed Virgin', 'John Eliot Gardiner', 1986);
 addToCollection('Larmes de Résurrection', 'La Tempête', 2018);
+addToCollection('Fashion Nugget', 'Cake', 1996);
 addToCollection('Bach Transcriptions', 'Leonard Slatkin', 2000);
-addToCollection('Guerre et Paix', 'Jordi Savall', 2015) 
-addToCollection('Californication', 'Red Hot Chili Peppers', 1999)
+addToCollection('Guerre et Paix', 'Jordi Savall', 2015);
+addToCollection('Californication', 'Red Hot Chili Peppers', 1999);
+addToCollection('Blood Sugar Sex Magik', 'Red Hot Chili Peppers', 1991);
+addToCollection('Comfort Eagle', 'Cake', 2001);
 console.log('The collection:', collection);
 
+// Function showCollection
 function showCollection(array) {
     console.log('Showing collection of ' + array.length + ' albums:' );
     for (let x of array) {
@@ -53,4 +58,31 @@ function showCollection(array) {
     }
 } // End function showCollection
 
+// Test showCollection
 showCollection(collection);
+
+// Function findArtist
+function findArtist(array, artist) {
+    let results = [];
+    for (let x of array) {
+
+        if (artist === x.artist) {
+            results.push(x);
+        }
+    }
+    console.log('Results for search ' + artist);
+    if (results.length === 0) {
+        console.log('No results.')
+    } else {
+    console.log(results);
+    }
+    return results;
+} // End function findArtist
+
+// Test findArtist
+findArtist(collection, 'Cake');
+findArtist(collection, 'Red Hot Chili Peppers');
+findArtist(collection, 'Red hot Chili Peppers'); 
+findArtist(collection, 'Jordi Savall');
+findArtist(collection, 'Voces8');
+
